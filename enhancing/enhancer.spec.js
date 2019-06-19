9aff57764f9d177067e2642a373436e7663d0088
Kristea Gamel <krgamel@gmail.com>
2019-06-19 18:45:50 -0500

    Diandra info

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,22 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('the enchancer', () => {
+
+    describe('the repair function', () => {
+        it('should restore the durability', () => {
+            //arrange-> mock data
+            const longsword = {};
+            const bow = {};
+
+            //act -> pass it into the function
+            const result = enhancer.repair(longsword);
+
+            //assert -> 
+        });
+    });
+});
+
+// A.A.A Method
+//Arrange
+//Act
+//Assert
